a8e91612bf53b7186df2ded548904306140f2fbb
gentom <gento.m.developer@gmail.com>
2017-08-28 23:43:00 +0900

    commit

--- test/test_markov_textgen.py
+++ test/test_markov_textgen.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-'''
 import sys
 import os
 sys.path.append(os.pardir)
-'''
 from mc import markov_textgen
 
 input_file = open('./data/test_markov_textgen.txt')
