7cb7aabb3d1e401f428a9857ec53157f0dfa46c3
gentom <gento.m.developer@gmail.com>
2017-08-29 16:07:23 +0900

    commit

--- mc/chain.py
+++ mc/chain.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+from random import choice, randint, random
 
-
-class MarkovChain:
-    pass
+# Work In Progress.
+class Chain:
+    def __init__(self, sets, t_matrix, step):
+        self.current = choice(sets)
